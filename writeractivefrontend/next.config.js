3e4a453f628062340b2fabbad46cc3b4f8c446d7
luis <lmatosgarcia2@gmail.com>
2022-07-31 03:39:10 -0400

    NExt JS Standalone

--- writeractivefrontend/next.config.js
+++ writeractivefrontend/next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: "standalone",
   env: {
     NEXT_PUBLIC_SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:8080/api',
   },
