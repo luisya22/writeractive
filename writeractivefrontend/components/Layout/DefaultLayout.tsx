810e6a6b85ef436888ac2aa33fb23a319cededee
luis <lmatosgarcia2@gmail.com>
2022-07-31 13:01:27 -0400

    Add Footer attribution

--- writeractivefrontend/components/Layout/DefaultLayout.tsx
+++ writeractivefrontend/components/Layout/DefaultLayout.tsx
@@ -1,6 +1,7 @@
 import styles from "../../styles/Home.module.scss";
 import Image from "next/image";
 import {MainNavbar} from "@/components/Layout/MainNavbar";
+import Link from "next/link";
 
 export default function DefaultLayout(props:{
     children: any
@@ -15,16 +16,7 @@ export default function DefaultLayout(props:{
             <main className={'min-h-screen flex flex-col justify-center overflow-hidden relative'}>
                 {props.children}
                 <footer className={styles.footer}>
-                    <a
-                        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Powered by{' '}
-                        <span className={styles.logo}>
-                            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-                        </span>
-                    </a>
+                    <p>Made by Luis Matos for the <Link href={'https://planetscale.com'}><span className={'text-blue-700 cursor-pointer'}>#PlanetScale</span></Link> and <Link href={'https://hashnode.com'}><span className={'text-blue-700 cursor-pointer'}>#Hashnode</span></Link>  Hackathon</p>
                 </footer>
             </main>
         </>
