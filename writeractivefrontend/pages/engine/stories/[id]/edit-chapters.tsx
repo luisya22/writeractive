cbd09b9cec53200e382141b9412c569f4990d16a
luis <lmatosgarcia2@gmail.com>
2022-07-31 00:06:11 -0400

    Add Zoom to chapter edit

--- writeractivefrontend/pages/engine/stories/[id]/edit-chapters.tsx
+++ writeractivefrontend/pages/engine/stories/[id]/edit-chapters.tsx
@@ -384,7 +384,6 @@ export default function EnginePage(props: any){
                             <button className={'flex items-center text-white'}><img className={'w-5 h-5 mr-1'} src={'/zoom-out.png'} alt={'plusIcon'}
                             />Zoom Out</button>
                         </div>
-                        <p className={'text-white'}>{zoom}</p>
                     </div>
 
                 </nav>
